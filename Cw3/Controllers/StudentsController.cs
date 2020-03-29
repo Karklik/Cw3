30d09f50532512549dfd2bac4f6b3e936249ea72
Karklik <17294332+Karklik@users.noreply.github.com>
2020-03-29 17:12:58 +0200

    Task 4: Added method parameterized by url segment

--- Cw3/Controllers/StudentsController.cs
+++ Cw3/Controllers/StudentsController.cs
@@ -15,5 +15,19 @@ namespace Cw3.Controllers
         {
             return "A, B, C";
         }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            } else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+
+            return NotFound("Nie znaleziono studneta");
+        }
     }
 }
